2a2eb7f5dfc211161c70ba4004b8260636e50eb2
Jody Mustafa <jodymustafa@USF-WiFi-224-188-155.laptops.usf.edu>
2025-03-08 14:04:59 -0500

    Added Admin model with validations and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,11 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Admin.create!(
+  name: "Super Admin",
+  email: "admin@example.com",
+  password: "securepassword",
+  password_confirmation: "securepassword",
+  role: "superadmin"
+)
+
