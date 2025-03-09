8d276b9e6c0608d7153e12e87b3b8a4518edc846
patrick <patbied@gmail.com>
2025-03-09 15:43:42 -0400

    Updating user_schedule_Model to correctly reference foreign key for courses

--- app/models/user_schedule.rb
+++ app/models/user_schedule.rb
@@ -1,4 +1,7 @@
 class UserSchedule < ApplicationRecord
+  # User Schedule maps a many to many relationship
+  # A student can have many courses and a course can have many students
   belongs_to :student
-  belongs_to :course
+  # Need to modify the foreign key so RoR knows we changed it to "CRN"
+  belongs_to :course, foreign_key: "course_id", primary_key: "CRN"
 end
