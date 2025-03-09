4a3c2e6d43b2de6653f618d773b61dbb7a77071f
Jody Mustafa <jodymustafa@USF-WiFi-224-189-40.laptops.usf.edu>
2025-03-09 15:51:57 -0400

    Updated admin_test.rb with test cases

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save admin without email" do
+    admin = Admin.new(name: "Test Admin", password: "password", role: "superadmin")
+    assert_not admin.save, "Saved the admin without an email"
+  end
+
+  test "should save admin with valid attributes" do
+    admin = Admin.new(name: "Test Admin", email: "admin@example.com", password: "password", role: "superadmin")
+    assert admin.save, "Failed to save admin with valid attributes"
+  end
 end
