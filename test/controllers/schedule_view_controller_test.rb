aba0bafb0a6b3ed4a2df5bc453114ff93439ae37
patrick <patbied@gmail.com>
2025-03-25 15:37:51 -0400

    Added tests for schedule controller

--- test/controllers/schedule_view_controller_test.rb
+++ test/controllers/schedule_view_controller_test.rb
@@ -5,4 +5,20 @@ class ScheduleViewControllerTest < ActionDispatch::IntegrationTest
     get schedule_view_index_url
     assert_response :success
   end
+
+
+  test "should create user_schedule" do
+    assert_difference("UserSchedule.count", 1) do
+      post schedule_view_index_url, params: { course_id: 102 }
+    end
+    assert_redirected_to schedule_view_index_url
+  end
+
+  test "should destroy user_schedule" do
+    user_schedule = user_schedules(:one)
+    assert_difference("UserSchedule.count", -1) do
+      delete schedule_view_url(user_schedule)
+    end
+    assert_redirected_to schedule_view_index_url
+  end
 end
