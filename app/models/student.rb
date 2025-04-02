4e006ab2ea0b74262a7d8c92a0e518bf6ac90285
Yaniel Gonzalez <yanielgonzalez@MacBookAir.lan>
2025-04-01 21:55:21 -0400

    Added major dropdown and fixed signup feedback

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,9 @@ class Student < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :credits_earned, numericality: { greater_than_or_equal_to: 0 }
-  validates :major, presence: true # Optional validation
+  validates :major, presence: true 
+  validates :password, presence: true, confirmation: true
+
 
   # Associations
   has_many :user_schedules
